9146b01d1117e5ec9e0b942f89e7f25a46c1aae7
saikumar130652 <48275735+saikumar130652@users.noreply.github.com>
2019-05-04 15:50:13 +0530

    Test code pushed to repository.

---
+++ Extract second last digit /Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args) {
+    Scanner sc =new Scanner(System.in);
+      int n=sc.nextInt();
+      int n1=n/10;
+      int n2=n1%10;
+      System.out.println(n2);
+	}
+}
